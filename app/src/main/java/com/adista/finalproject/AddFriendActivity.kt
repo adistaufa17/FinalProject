cf4bb6ed4ccf6791aa1c39adf6b6829ddd8cf1ef
adistaufa17 <adistanurulfadilah01@gmail.com>
2024-09-06 03:30:21 +0700

    revisi 2

--- app/src/main/java/com/adista/finalproject/AddFriendActivity.kt
+++ app/src/main/java/com/adista/finalproject/AddFriendActivity.kt
@@ -10,7 +10,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
